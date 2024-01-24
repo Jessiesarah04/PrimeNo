765cd23bbc736f86a9eeeb78c61f761444dc6d0e
01fe22bca057 <samuelkotikela14@gmail.com>
2024-01-24 20:34:04 +0530

    PrimeNumber

---
+++ PrimeNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PrimeNumber{
+    static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int j = 2; j<=num/j; j++) {
+            if (num % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the starting number: ");
+        int Start = sc.nextInt();
+        System.out.print("Enter the ending number: ");
+        int end = sc.nextInt();
+    
+        for (int i = Start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        sc.close();
+    }
+}

0ef1a8e93040545de32adc694cd2baf4bbb6254e
01fe22bca057 <samuelkotikela14@gmail.com>
2024-01-25 21:07:53 +0530

    code for Fibonacci series

---
+++ Fibonacciseries.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Fibonacciseries{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of terms for Fibonacci series: ");
+        int n = sc.nextInt();
+
+        System.out.println("Fibonacci series up to " + n + " terms:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(Fibonacci(i) + " ");
+        }
+
+        sc.close();
+    }
+    static int Fibonacci(int n){
+        if (n <= 1) {
+            return n;
+        } else {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+}
+}
